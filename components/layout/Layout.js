88c4660a3f5f2a77f5bcd0ca8533e5e7fe9aac79
JakubSmela <jakub_smela@outlook.com>
2022-12-20 13:06:17 +0100

    formatting corrected

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,17 @@
-import Navigation from "./Navigation"
+import Navigation from "./Navigation";
 
 function Layout(props) {
-    return (
-      <div style={{height: "100vh", backgroundImage: "linear-gradient(blue, black)"}}>
-        <Navigation/>
-        <main >{props.children}</main>
-      </div>
-    );
-  }
-  
-  export default Layout;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        backgroundImage: "linear-gradient(blue, black)",
+      }}
+    >
+      <Navigation />
+      <main>{props.children}</main>
+    </div>
+  );
+}
+
+export default Layout;
--- pages/challenges/index.tsx
+++ pages/challenges/index.tsx
@@ -1,140 +1,197 @@
-import {ChallengeDTO, ChallengeType, LanguageName} from "../../Challenge.dto";
-import {useEffect, useState} from "react";
+import { ChallengeDTO, ChallengeType, LanguageName } from "../../Challenge.dto";
+import { useEffect, useState } from "react";
 import Editor from "@monaco-editor/react";
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link'
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
 import { useRouter } from "next/router";
-import { Card, Button } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Layout from "./../../components/layout/Layout"
-
+import { Card, Button } from "@mui/material";
+import Layout from "./../../components/layout/Layout";
 
 export default function Home() {
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const [challengeList, setChallengeList] = useState<ChallengeDTO[]>([]);
-    const [currentChallenge, setCurrentChallenge] = useState<ChallengeDTO>(defaultChallengeDTO);
-    const [code, setCode] = useState<string>("");
-    const [language, setLanguage] = useState<string>("javascript");
-    const [testResults, setTestResults] = useState<string>("");
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [challengeList, setChallengeList] = useState<ChallengeDTO[]>([]);
+  const [currentChallenge, setCurrentChallenge] =
+    useState<ChallengeDTO>(defaultChallengeDTO);
+  const [code, setCode] = useState<string>("");
+  const [language, setLanguage] = useState<string>("javascript");
+  const [testResults, setTestResults] = useState<string>("");
 
-    const codeChangeHandler = (newCode: string | undefined) => {
-        setCode(newCode ?? "");
+  const codeChangeHandler = (newCode: string | undefined) => {
+    setCode(newCode ?? "");
+  };
+
+  function returnToMenuHandler() {
+    router.push("/");
+  }
+
+  useEffect(() => {
+    fetch("/api/challenge/all")
+      .then((data) => {
+        return data.json();
+      })
+      .then((challengeList) => {
+        setChallengeList(challengeList);
+        setCurrentChallenge(challengeList[0]);
+        setLanguage(challengeList[0].language.toLowerCase());
+        setCode(challengeList[0]?.codeSnippet);
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (currentChallenge === undefined) {
+      return;
     }
+    console.log(currentChallenge);
+    setLanguage(currentChallenge.language.toLowerCase());
+    setCode(currentChallenge?.codeSnippet);
+  }, [currentChallenge]);
 
-    function returnToMenuHandler() {
-        router.push('/')
-    }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    useEffect(() => {
-        fetch("/api/challenge/all")
-            .then(data => {return data.json()})
-            .then(challengeList => {
-                setChallengeList(challengeList);
-                setCurrentChallenge(challengeList[0]);
-                setLanguage(challengeList[0].language.toLowerCase());
-                setCode(challengeList[0]?.codeSnippet);
-                setIsLoading(false)
-            })
-    }, [])
+  const submitCode = () => {
+    fetch("/api/attempt/perform", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        challengeId: currentChallenge.challengeId,
+        code: code,
+      }),
+    })
+      .then((result) => result.text())
+      .then((text) => setTestResults(text));
+  };
 
-   
+  return (
+    <>
+      <Layout>
+        <div style={{ height: "40vh" }}>
+          <Card
+            sx={{
+              borderRadius: "20px",
+              paddingY: "5px",
+              paddingX: "10px",
+              marginX: "3rem",
+              marginY: "2vh",
+              borderColor: "black",
+              heightMax: "18vh",
+              width: "65rem",
+              float: "left",
+            }}
+          >
+            <h1 style={{ marginTop: "0px" }}>
+              <span style={{ color: "purple", fontWeight: "bold" }}>
+                {currentChallenge?.name}
+              </span>
+            </h1>
+            <ul>
+              <li>
+                <span style={{ color: "purple", fontWeight: "bold" }}>
+                  Task:
+                </span>{" "}
+                {currentChallenge?.description}
+              </li>
+              <li>
+                <span style={{ color: "purple", fontWeight: "bold" }}>
+                  Programming language:
+                </span>{" "}
+                {currentChallenge?.language}
+              </li>
+              <li>
+                <span style={{ color: "purple", fontWeight: "bold" }}>
+                  Difficulty level:
+                </span>{" "}
+                {currentChallenge?.type.toLowerCase()}
+              </li>
+            </ul>
+            <div role="presentation" style={{ margin: "auto" }}>
+              <Breadcrumbs aria-label="breadcrumb">
+                <span style={{ color: "purple", fontWeight: "bold" }}>
+                  Tasks:
+                </span>
 
-
-    useEffect(() => {
-        if (currentChallenge === undefined) {
-            return;
-        }
-        console.log(currentChallenge);
-        setLanguage(currentChallenge.language.toLowerCase());
-        setCode(currentChallenge?.codeSnippet);
-    }, [currentChallenge])
-
-    if(isLoading){
-        return (
-            <div>
-                Loading...
+                {challengeList.map((challenge) => (
+                  <Link
+                    underline="hover"
+                    color="inherit"
+                    onClick={() => setCurrentChallenge(challenge)}
+                  >
+                    {challenge.name}
+                  </Link>
+                ))}
+              </Breadcrumbs>
             </div>
-        )
-    }
+          </Card>
 
+          <Card
+            sx={{
+              borderRadius: "20px",
+              marginRight: "3rem",
+              marginY: "2vh",
+              padding: "10px",
+              borderColor: "black",
+              width: "40rem",
+              height: "80vh",
+              float: "right",
+            }}
+          >
+            <li>
+              <span style={{ color: "purple", fontWeight: "bold" }}>
+                Example test case:
+              </span>{" "}
+              {currentChallenge?.exampleTestCases}
+            </li>
+          </Card>
 
-
-    const submitCode = () => {
-        fetch("/api/attempt/perform",
-            {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+          <Card
+            sx={{
+              borderRadius: "20px",
+              marginLeft: "3rem",
+              marginY: "3vh",
+              borderColor: "black",
+              float: "left",
+            }}
+          >
+            <Editor
+              height="58vh"
+              width="65rem"
+              theme="vs-dark"
+              value={code}
+              language={language}
+              options={{
+                minimap: {
+                  enabled: false,
                 },
-                body: JSON.stringify({
-                        challengeId: currentChallenge.challengeId,
-                        code: code
-                    }
-                )
-            }).then(result => result.text())
-            .then(text => setTestResults(text));
-    }
+              }}
+              onChange={(value, ev) => codeChangeHandler(value)}
+            />
+          </Card>
+        </div>
 
-    return (<>
-            <Layout>
-              <div style={{height: "40vh"}}>
-                <Card sx={{borderRadius: "20px", paddingY: "5px", paddingX: "10px", marginX :"3rem", marginY:"2vh", borderColor: "black", heightMax: "18vh", width:"65rem", float: "left"}}>
-                <h1 style={{marginTop: "0px"}}><span style={{color: "purple", fontWeight: "bold"}}>{currentChallenge?.name}</span></h1>
-                <ul>
-                    <li><span style={{color: "purple", fontWeight: "bold"}}>Task:</span> {currentChallenge?.description}</li>
-                    <li><span style={{color: "purple", fontWeight: "bold"}}>Programming language:</span> {currentChallenge?.language}</li>
-                    <li><span style={{color: "purple", fontWeight: "bold"}}>Difficulty level:</span> {currentChallenge?.type.toLowerCase()}</li>
-                </ul>
-                <div role="presentation" style={{margin: "auto"}}>
-                    <Breadcrumbs aria-label="breadcrumb">
-                    <span style={{color: "purple", fontWeight: "bold"}}>Tasks:</span>
+        <div
+          style={{ marginLeft: "50rem", marginRight: "50rem", clear: "both" }}
+        >
+          <Button
+            sx={{ paddingY: "10px", paddingX: "20px", borderRadius: "15px" }}
+            variant="outlined"
+            onClick={submitCode}
+          >
+            Submit Challenge!
+          </Button>
+        </div>
 
-                    {challengeList.map((challenge) => (
-                        <Link underline="hover" color="inherit" onClick={() => setCurrentChallenge(challenge)}>
-                        {challenge.name}
-                        </Link>))}
-                        
-                    </Breadcrumbs>
-                </div>
-                </Card>
-
-                <Card sx={{borderRadius: "20px", marginRight :"3rem", marginY:"2vh", padding: "10px", borderColor: "black", width:"40rem", height:"80vh", float: "right"}}>
-                <li><span style={{color: "purple", fontWeight: "bold"}}>Example test case:</span> {currentChallenge?.exampleTestCases}</li>
-                </Card>
-                
-                <Card sx={{borderRadius: "20px", marginLeft :"3rem", marginY:"3vh", borderColor: "black",  float: "left"}}>
-                <Editor
-                    height="58vh"
-                    width="65rem"
-                    theme="vs-dark"
-                    value={code}
-                    language={language}
-                    options={{
-                        minimap: {
-                            enabled: false
-                        }
-                    }}
-                    onChange={(value, ev) => codeChangeHandler((value))}
-                />
-                </Card>
-              </div>
-            
-
-            
-
-            <div style={{marginLeft: "50rem", marginRight: "50rem", clear: "both"}}>
-                <Button sx={{paddingY: "10px", paddingX: "20px", borderRadius: "15px", }} variant="outlined" onClick={submitCode}>Submit Challenge!</Button>
-        
-            </div>
-            
-            
-            
-            <div style={{color: "blue", fontSize: "20px", float: "left"}}>{testResults}</div>
-          </Layout>  
-        </>
-    )
+        <div style={{ color: "blue", fontSize: "20px", float: "left" }}>
+          {testResults}
+        </div>
+      </Layout>
+    </>
+  );
 }
 
 const defaultChallengeDTO: ChallengeDTO = {
@@ -144,8 +201,5 @@ const defaultChallengeDTO: ChallengeDTO = {
   exampleTestCases: "",
   language: LanguageName.JAVASCRIPT,
   name: "",
-  type: ChallengeType.EASY
-}
-
-
-
+  type: ChallengeType.EASY,
+};
